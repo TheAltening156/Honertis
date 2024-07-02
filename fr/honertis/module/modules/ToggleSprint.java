7d0720bc622e9a1fe7202743de76f15d9a5d3096
TheAltening156 <90117489+TheAltening156@users.noreply.github.com>
2024-07-02 11:37:48 +0200

    ToggleSprint Fix

--- fr/honertis/module/modules/ToggleSprint.java
+++ fr/honertis/module/modules/ToggleSprint.java
@@ -9,10 +9,5 @@ public class ToggleSprint extends ModuleBase{
 	public ToggleSprint() {
 		super("ToggleSprint", "Sprint automatiquement", Category.UTILITIES);
 	}
-		
-	@Override
-	public void onUpdate(EventUpdate e) {
-		mc.thePlayer.setSprinting(mc.gameSettings.keyBindForward.isKeyDown() && !mc.thePlayer.isCollidedHorizontally && !(mc.thePlayer.isEating() || mc.thePlayer.isBlocking()));
-	}
 	
 }
