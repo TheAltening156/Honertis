7c03d0e1b9ded4921bd935daf528455047130832
TheAltening156 <90117489+TheAltening156@users.noreply.github.com>
2024-10-27 07:46:40 +0100

    Fix discord rpc (test)

--- fr/honertis/DiscordRPCInit.java
+++ fr/honertis/DiscordRPCInit.java
@@ -1,13 +1,31 @@
 package fr.honertis;
 
-import club.minnced.discord.rpc.DiscordEventHandlers;
-import club.minnced.discord.rpc.DiscordRPC;
-import club.minnced.discord.rpc.DiscordRichPresence;
+import net.arikia.dev.drpc.DiscordEventHandlers;
+import net.arikia.dev.drpc.DiscordRPC;
+import net.arikia.dev.drpc.DiscordRichPresence;
 
 public class DiscordRPCInit {
 	public static String id = "1299778561762398209";
 	public static void init() {
-		DiscordRPC rpc = DiscordRPC.INSTANCE;
+		 DiscordEventHandlers handlers = new DiscordEventHandlers.Builder().setReadyEventHandler((user) -> {
+            DiscordRichPresence.Builder presence = new DiscordRichPresence.Builder("UWU !!1!!111!11!!1!");
+            presence.setDetails("Running my Honertis");
+            presence.setSmallImage("png", "hehe");
+            presence.setDetails("Test");
+            presence.setStartTimestamps(System.currentTimeMillis() / 1000);
+            DiscordRPC.discordUpdatePresence(presence.build());
+        }).build();
+        DiscordRPC.discordInitialize(id, handlers, false);
+        DiscordRPC.discordRegister(id, "");
+        new Thread(() -> {
+			while (!Thread.currentThread().isInterrupted()) {
+				DiscordRPC.discordRunCallbacks();
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {}
+			}
+		}, "RPC-Callback").start();
+		/*DiscordRPC rpc = DiscordRPC.INSTANCE;
 		DiscordEventHandlers h = new DiscordEventHandlers();
 		h.ready = (user) -> System.out.println("Ready !");
 		rpc.Discord_Initialize(id, h, true, "");
@@ -24,6 +42,6 @@ public class DiscordRPCInit {
 					Thread.sleep(2000);
 				} catch (InterruptedException e) {}
 			}
-		}, "RPC-Callback").start();
+		}, "RPC-Callback").start();*/
 	}
 }
