4a99670ca42a48bfb6ae963ab01bf2c28dca9c43
TheAltening156 <miaoumiaou153@gmail.com>
2024-11-08 16:24:16 +0100

    Modif CPS & FPS

--- fr/honertis/module/modules/CPS.java
+++ fr/honertis/module/modules/CPS.java
@@ -11,55 +11,57 @@ import fr.honertis.module.Category;
 import fr.honertis.module.ModuleBase;
 import net.minecraft.client.gui.Gui;
 
-public class CPS extends ModuleBase{
-	public List<Long> lCps = Lists.newArrayList();
-	public boolean lPressed;
-	public long lLastPressed;
-	public List<Long> rCps = Lists.newArrayList();
-	public boolean rPressed;
-	public long rLastPressed;
-	public int posX = 10;
-	public int posY = 30;
-	
-	public CPS() {
-		super("CPS", "Montre combien de cps vous faites en 1 seconde", Category.UTILITIES);
-	}
-	
-	@Override
-	public void onGuiRender(EventRenderGui e) {
-		if (mc.currentScreen == null) {
-			boolean lPressed = Mouse.isButtonDown(0);
-			if (lPressed != this.lPressed) {
-				this.lLastPressed = System.currentTimeMillis();
-				this.lPressed = lPressed;
-				if (lPressed) {
-					this.lCps.add(this.lLastPressed);
-				}
-			}
-			boolean rPressed = Mouse.isButtonDown(1);
-			if (rPressed != this.rPressed) {
-				this.rLastPressed = System.currentTimeMillis();
-				this.rPressed = rPressed;
-				if (rPressed) {
-					this.rCps.add(this.rLastPressed);
-				}
-			}
-		}
-		String lmb = "LMB: " + getLCps();
-		String rmb = "RMB: " + getRCps();
-		String text = lmb + " " + rmb;
-		Gui.drawRect(posX - 4, posY - 4, posX + mc.fontRendererObj.getStringWidth(text) + 4, posY + 11, 0x90000000);
-		mc.fontRendererObj.drawStringWithShadow(text, posX, posY, -1);
-	}
-	
-	public int getLCps() {
-		long time = System.currentTimeMillis();
-		this.lCps.removeIf(aLong -> aLong + 1000 < time);
-		return  this.lCps.size();
-	}
-	public int getRCps() {
-		long time = System.currentTimeMillis();
-		this.rCps.removeIf(aLong -> aLong + 1000 < time);
-		return  this.rCps.size();
-	}
+public class CPS extends ModuleBase {
+    public List<Long> lCps = Lists.newArrayList();
+    public List<Long> rCps = Lists.newArrayList();
+    public int posX = 10;
+    public int posY = 30;
+    
+    private PressState leftButtonState;
+    private PressState rightButtonState;
+    
+    public CPS() {
+        super("CPS", "Montre combien de cps vous faites en 1 seconde", Category.UTILITIES);
+        leftButtonState = new PressState(lCps);
+        rightButtonState = new PressState(rCps);
+    }
+    
+    @Override
+    public void onGuiRender(EventRenderGui e) {
+        leftButtonState.isKeyPressed(Mouse.isButtonDown(0));
+        rightButtonState.isKeyPressed(Mouse.isButtonDown(1));
+
+        String lmb = "LMB: " + lCps.size();
+        String rmb = "RMB: " + rCps.size();
+        String text = lmb + " " + rmb;
+        
+        Gui.drawRect(posX - 4, posY - 4, posX + mc.fontRendererObj.getStringWidth(text) + 4, posY + 11, 0x90000000);
+        mc.fontRendererObj.drawStringWithShadow(text, posX, posY, -1);
+    }
+    
+    public class PressState {
+        public boolean pressed;
+        public long lastPress;
+        public List<Long> list;
+
+        PressState(List<Long> list) {
+            this.list = list;
+        }
+
+        public void isKeyPressed(boolean key) {
+            if (key != pressed) {
+                lastPress = System.currentTimeMillis();
+                pressed = key;
+                if (key) {
+                    list.add(lastPress);
+                }
+            }
+            cleanOldClicks();
+        }
+
+        private void cleanOldClicks() {
+            long currentTime = System.currentTimeMillis();
+            list.removeIf(aLong -> aLong + 1000 < currentTime);
+        }
+    }
 }
--- fr/honertis/module/modules/FPS.java
+++ fr/honertis/module/modules/FPS.java
@@ -1,8 +1,13 @@
 package fr.honertis.module.modules;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 import fr.honertis.event.EventRenderGui;
 import fr.honertis.module.Category;
 import fr.honertis.module.ModuleBase;
+import fr.honertis.settings.BooleanSettings;
 import net.minecraft.client.gui.Gui;
 
 public class FPS extends ModuleBase{
@@ -10,16 +15,23 @@ public class FPS extends ModuleBase{
 			   posY = 50,
 			   oldX,
 			   oldY;
+	public List<Long> list = Lists.newArrayList();
 	
+	public BooleanSettings realTime = new BooleanSettings("Temps Réel", false);
 	public FPS() {
 		super("FPS", "Montrer vos FPS actuels", Category.UTILITIES);
+		this.addSettings(realTime);
 	}
 	
 	@Override
 	public void onGuiRender(EventRenderGui e) {
 		posX = posY = 10;
-		Gui.drawRect(posX - 4, posY - 4, posX + mc.fontRendererObj.getStringWidth("FPS : " + mc.getDebugFPS()) + 4, posY + 11, 0x90000000);
-		mc.fontRendererObj.drawStringWithShadow("FPS : " + mc.getDebugFPS(), posX, posY, -1);
+		String name = "FPS : " + (realTime.isToggled() ? list.size() : mc.getDebugFPS());
+		Gui.drawRect(posX - 4, posY - 4, posX + mc.fontRendererObj.getStringWidth(name) + 4, posY + 11, 0x90000000);
+		mc.fontRendererObj.drawStringWithShadow(name, posX, posY, -1);
+		
+		list.add(System.currentTimeMillis());
+		list.removeIf(aLong -> aLong + 1000 < System.currentTimeMillis());
 	}
 	
 }
