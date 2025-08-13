172468e55ec11702c9ec045ff9156f7ee9e89452
TheAltening156 <90117489+TheAltening156@users.noreply.github.com>
2025-08-13 19:33:41 +0200

    nouveau bouton

--- net/minecraft/client/gui/GuiMainMenu.java
+++ net/minecraft/client/gui/GuiMainMenu.java
@@ -19,6 +19,7 @@ public class GuiMainMenu extends GuiScreen {
 			System.out.println("Nouvelle version de Honertis disponible ! https://bit.ly/honertis");
 			this.buttonList.add(new GuiButton(0, this.width / 2 - 105, this.height / 2 + 40, 100, 20, "Mettre à jour"));
 			this.buttonList.add(new GuiButton(1, this.width / 2 + 5, this.height / 2 + 40, 100, 20, "Ignorer"));
+			this.buttonList.add(new GuiButton(2, this.width / 2 - 105, this.height / 2 + 62, 210, 20, "Vous pouvez aussi utiliser le launcher !"));
 		} else {
 			this.mc.displayGuiScreen(new McMainMenu());
 		}
@@ -28,9 +29,18 @@ public class GuiMainMenu extends GuiScreen {
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		drawBackground(0);
 		super.drawScreen(mouseX, mouseY, partialTicks);
+
 		mc.fontRendererObj.drawCenteredString("Nouvelle version de Honertis disponible !", this.width / 2, this.height / 2 - 50, -1);
 		mc.fontRendererObj.drawCenteredString("Vous êtes toujours en " + instance.version + ", la version actuelle est la " + WebUtils.currentVersion, this.width / 2, this.height / 2 - 50 + mc.fontRendererObj.FONT_HEIGHT, -1);
-		if (update) mc.fontRendererObj.drawCenteredString("Ouverture de la page web ...", this.width / 2, this.height / 2 + 1, -1);
+		if (update) {
+			mc.fontRendererObj.drawCenteredString("Ouverture de la page web ...", this.width / 2, this.height / 2 + 1, -1);
+			try {
+				Thread.sleep(1500L);
+				update = false;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	@Override
@@ -40,11 +50,16 @@ public class GuiMainMenu extends GuiScreen {
 				update = true;
 				WebUtils.browseWebsite("https://bit.ly/honertis");	
 			}
+			if (button.id == 2) {
+				update = true;
+				WebUtils.browseWebsite("https://github.com/TheAltening156/HonertisLauncher/releases/");
+			}
 		}
 		if (button.id == 1) {
 			instance.update = false;
 			this.mc.displayGuiScreen(new McMainMenu());
 		}
+		
 	}
 	
 }
