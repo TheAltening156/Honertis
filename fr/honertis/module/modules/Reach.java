d0a47fb2411da27121684b704892ce7ca232d577
TheAltening156 <90117489+TheAltening156@users.noreply.github.com>
2026-07-19 16:22:18 +0200

    Reach by hitting, not cps

--- fr/honertis/module/modules/Reach.java
+++ fr/honertis/module/modules/Reach.java
@@ -1,20 +1,24 @@
 package fr.honertis.module.modules;
 
-import java.util.stream.Collectors;
-
 import fr.honertis.event.EventAttack;
+import fr.honertis.event.EventReceivePacket;
 import fr.honertis.event.EventRenderGui;
 import fr.honertis.module.Category;
 import fr.honertis.module.ModuleBase;
 import fr.honertis.utils.Utils;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.entity.Entity;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S19PacketEntityStatus;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
 
 public class Reach extends ModuleBase{
 	public float lastReach = 0;
+	private float pendingReach;
+	private int sentAttack = -1;
 	
 	public Reach() {
 		super("ReachIndicator", "module.reachIndicator", Category.UTILITIES);
@@ -35,12 +39,32 @@ public class Reach extends ModuleBase{
 	@Override
 	public void onAttacking(EventAttack e) {
 		if (e.isPost()) {
-			MovingObjectPosition mop = mc.objectMouseOver;
-			if (mop != null) {
-				Vec3 eyes = mc.thePlayer.getPositionEyes(1.0F);
-		    	double reach = eyes.distanceTo(mop.hitVec);
-				lastReach = (float) (reach <= 6 ? reach : lastReach);
-			}
-		}
+	        MovingObjectPosition mop = mc.objectMouseOver;
+	        if (mop != null && mop.entityHit != null) {
+	            Vec3 eyes = mc.thePlayer.getPositionEyes(1.0F);
+	            double reach = eyes.distanceTo(mop.hitVec);
+
+	            if (reach <= 6) {
+	                pendingReach = (float) reach;
+	                sentAttack = mop.entityHit.getEntityId();
+	            }
+	        }
+	    }
+	}
+	@Override
+	public void onReceivePacket(EventReceivePacket e) {
+	    Packet packet = e.getPacket();
+	    if (!(packet instanceof S19PacketEntityStatus)) return;
+
+	    S19PacketEntityStatus s19 = (S19PacketEntityStatus) packet;
+	    if (s19.getOpCode() != 2) return;
+
+	    Entity target = s19.getEntity((World) this.mc.theWorld);
+	    if (target == null) return;
+
+	    if (this.sentAttack != -1 && target.getEntityId() == this.sentAttack) {
+	        this.sentAttack = -1;
+	        this.lastReach = pendingReach; // seulement ici, sur confirmation serveur
+	    }
 	}
 }
